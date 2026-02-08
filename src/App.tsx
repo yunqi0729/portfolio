46c32e027129ba87ce8650ce6f79fef368151d36
7ncky7 <yunqi0729@gmail.com>
2026-02-08 21:04:58 +0800

    Update to use Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { format } from "date-fns"
 function App() {
   return (
     <>
-      <h1>Welcome to my portfolio!</h1>
-      <p>Today's Date: {format(new Date(), "dd/MM/yyyy")}</p>
+      <h1 className="text-2xl text-gray-700 m-4 font-bold">Welcome to my portfolio!</h1>
+      <p className="text-sm text-gray-400 m-4">Today's Date: {format(new Date(), "dd/MM/yyyy")}</p>
     </>
   )
 }
